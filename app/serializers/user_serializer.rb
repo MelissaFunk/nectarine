916a11785b85f62a37d92a837d753d5c2032642f
Melissa Funk <melissafunk85@gmail.com>
2022-11-28 16:05:48 -0800

    fixed grocery list

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :image, :groceries
 
   def groceries
-    non_made = self.object.recipes.select { |r| r.has_made === false && r.date === nil }
+    non_made = self.object.recipes.select { |r| r.date != nil }
     non_made.map(&:ingredients).join(", ").split(", ").uniq.sort
   end
 end
